a3c2ba8ed1dec4ebd0ed026a31590d96afebb262
andhikajansenreforwan <jansendhika21@gmail.com>
2024-10-21 20:15:01 +0700

    first commit

---
+++ dudeesseyo-script.js
@@ -0,0 +1,6 @@
+alert('Welcome To Dudeesseyo Website')
+
+var name = prompt("What's Your Name?");
+console.log(name);
+var hobby = prompt("What's your favorite Drama/Film Genre?")
+console.log(hobby)
